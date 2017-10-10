cd6d121207d1fb34e3bf7d66922a33cad1d2c430
sslayo <steve.slayo@gmail.com>
2017-10-10 22:11:03 +1100

    Update api.php

--- BinanceWebApplication/api.php
+++ BinanceWebApplication/api.php
@@ -1,4 +1,4 @@
 <?php 
-$apiKey="";
-$secretKey="";
+$apiKey=""; // This will update with your API key when you enter via this config or in the account tab on the web application 
+$secretKey=""; // This will update with your Secret key when you enter via this config or in the account tab on the web application 
 ?>
